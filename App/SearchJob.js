fa2dd0451ea04fc3cb748768ac2ecfbc9728a0d8
sampathkumar0078 <sampathkumar0078@gmail.com>
2019-10-18 11:50:19 +0530

    App folder added for deploying

---
+++ App/SearchJob.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var users = express.Router();
+var port = process.env.PORT || 3000;
+var database=require('./Database/DBConnections');
+var multer=require('multer');
+var nodemailer=require('nodemailer');
+var randomString=require('randomstring');
+process.env.secretkey='zyclyx';
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,'./uploads/Images')
+    },
+    filename: function (req, file, cb) {
+        var file=file.originalname;
+      cb(null,file )
+    }
+  })
+  var appData={
+    "error":1,
+    "data":""
+}
+   
+  var upload = multer({ storage: storage })
+
+  users.post('/Searchjob',upload.single(''),function(req,res){
+      var job=req.body.job;
+      database.connection.getConnection(function(err,connection){
+          if(err){
+              console.log(err)
+              appData['data']='Problem while connecting to database'
+              res.status(200).json(appData)
+          }
+          else{
+          connection.query('select * from jobs where jid like ?',['%'+job+'%'],function(err,data){
+            
+              if(err){
+                  console.log(err)
+                  appData['data']='Problem in database';
+                  res.status(200).json(appData)
+              }
+              else{
+                  console.log(data[0]);
+                  res.status(201).json(data)
+              }
+          })
+        }
+      })
+
+  })
+
+
+
+  users.post('/sample',upload.single(''),function(req,res){
+  // var username= req.body.username;
+  //var password=req.body.password;
+
+  const secret=randomString.generate(7);
+  console.log(secret+' random string')
+
+  // var mail= nodemailer.createTransport({
+  //    service:'gmail',
+  //    auth:{
+  //      user:username,
+  //      pass:password
+       
+  //    }
+ 
+
+  //  })
+  //  console.log(username,password)
+  //  var mailOptions=  {
+  //   from:'sampathkumar0078@gmail.com',
+  //   to:'sampathkumar0078@gmail.com',
+  //   subject:'Hello world',
+  //   text:'sample mail code using nodejs'
+  // };
+  // mail.sendMail(mailOptions,function(err,data){
+  //   if(err){
+  //     console.log(err);
+  //     appData['data']='Please provide valid username and password'
+  //     res.status(200).json(appData);
+
+  //   }
+  //   else{
+  //     console.log('mail sent')
+  //     appData='mail sent'
+  //     res.status(201).json(appData)
+  //   }
+
+  // })
+
+   
+  })
+
+  module.exports=users;
---
+++ App/Users.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require("body-parser");
+var users = express.Router();
+var port = process.env.PORT || 3000;
+var database=require('./Database/DBConnections');
+var multer=require('multer');
+var fs=require('fs');
+var http=require('http');
+users.use('/uploads',express.static('uploads'))
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,'./uploads/Images')
+    },
+    filename: function (req, file, cb) {
+        var file=file.originalname;
+      cb(null,file )
+    }
+  })
+   
+  var upload = multer({ storage: storage })
+
+function onRequest(req,res){
+    res.writeHead(200,{'content-type':'text/html'})
+ // res.write('Index.html');
+    fs.readFile('./Index.html',null,function(err,data){
+        if(err){
+            res.writeHead(404);
+            res.write('page not found')
+        }
+        else{
+           res.end(data);
+          
+
+        }
+    })
+    res.end();
+}
+
+users.post('/Register',upload.single('file'),function(req,res){
+
+   var firstname= req.body.firstname;
+   var lastname= req.body.lastname;
+    var mail=req.body.mail;
+    var phonenumber=req.body.phonenumber;
+    var gender=req.body.gender;
+    var country=req.body.country;
+    var qualification=req.body.qualification;
+    var expertise =req.body.expertise;
+    var experienceyears=req.body.experienceyears;
+    var experiencemonths=req.body.experiencemonths;
+    var position=req.body.position;
+    var currentcompany=req.body.currentcompany;
+    var currentctc=req.body.currentctc;
+    var expectedctc=req.body.expectedctc;
+    var noticeperiod=req.body.noticeperiod;
+    var currentlocation=req.body.currentlocation;
+    var file='http://192.168.1.56:3000/Images/'+req.file.originalname;
+    var additionalinformation=req.body.additionalinformation;
+
+    
+   
+    var experience=experienceyears+'.'+experiencemonths;
+    var name=firstname+' '+lastname;
+
+    console.log(name,mail,phonenumber,gender,country,qualification,expertise,experience,position,currentcompany,currentctc,expectedctc,noticeperiod,currentlocation,file,additionalinformation+'details are')
+    var appData = {
+        "error": 1,
+        "data": ""
+    };
+    database.connection.getConnection(function(err,connection){
+        if(err){
+            console.log('error in database');
+        }
+        else{
+            connection.query('select  * from register where mail=?',[mail],function(err,rows){
+                if(err){
+                    console.log(err);
+                    appData['data']='Error in database'
+                    res.status(200).json(appData);
+                }
+                else if(rows.length>0)
+                {
+                    console.log('email already exists');
+                    appData['data']='Email already exists';
+                    res.status(200).json(appData)
+                }
+                else{
+                    connection.query('insert into register values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[name,mail,phonenumber,gender,country,qualification,expertise,experience,position,currentcompany,currentctc,expectedctc,noticeperiod,currentlocation,file,additionalinformation],function(err,data){
+                        if(err){
+                            console.log(err);
+                        }
+                        else{
+                            console.log('values inserted')
+                            appData['data']='values inserted'
+                            res.status(201).json(appData);
+                        }
+                    })
+        
+                }
+            })
+            
+
+        }
+    })
+    
+
+
+})
+
+users.get('/data',function(req,res){
+    console.log('redirected')
+    res.send('redirected')
+})
+
+
+module.exports=users;
